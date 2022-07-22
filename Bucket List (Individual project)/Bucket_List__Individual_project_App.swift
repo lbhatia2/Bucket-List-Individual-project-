44a95dda395cf9f812a0eea5bc432a1f0530065a
Lina Bhatia <lina@bhatia.org>
2022-07-22 14:22:44 -0500

    Initial Commit

---
+++ Bucket List (Individual project)/Bucket_List__Individual_project_App.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_List__Individual_project_App.swift
+//  Bucket List (Individual project)
+//
+//  Created by Lina Bhatia on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_List__Individual_project_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
